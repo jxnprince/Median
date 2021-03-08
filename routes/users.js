a869b34ad7ca6feedd1abf7578a207e24d6c25e9
Robert George <rgeorge1982@gmail.com>
2021-03-08 17:33:43 -0500

    working on validations

--- routes/users.js
+++ routes/users.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const { csrfProtection, asyncHandler } = require('../utils.js');
 const { loginUser } = require('../auth.js');
 const { User } = require('../db/models');
+const { check, validationResult } = require('express-validator');
 
 
 
@@ -14,6 +15,36 @@ router.get("/", asyncHandler(async (req, res) => {
   }
 }));
 
+const createUserValidators =[
+  check('email')
+    .exists({checkFalsy: true})
+    .withMessage('Please input a valid email')
+    .isLength({ max : 100})
+    .withMessage('Please provide an email address under 100 characters'),
+  check('firstName')
+    .exists({checkFalsy: true})
+    .withMessage('Please input a valid first name')
+    .isLength({ max : 45})
+    .withMessage('Please provide a first name under 45 characters'),
+  check('lastName')
+    .exists({checkFalsy: true})
+    .withMessage('Please input a valid last name')
+    .isLength({ max : 75})
+    .withMessage('Please provide a last name under 75 characters'),
+  check('hashedPassword')
+    .exists({checkFalsy: true})
+    .withMessage('Please input a valid password')
+    .isLength({ max : 255})
+    .withMessage('Please provide a password under 255 characters'),
+  check('gender')
+    .isLength({ max : 20})
+    .withMessage('Please provide a gender under 20 characters'),
+  check('birthDate')
+    //some logic here
+];
+router.post('/submit', csrfProtection, asyncHandler((req, res) => {
+
+}))
 
 
 router.post("/demo-user", csrfProtection, asyncHandler(async(req, res) => {
