0c9a2c84a12c7d1d53173c36355961527dfce5fe
Troy <troyd9216@gmail.com>
2021-03-08 22:43:24 -0600

    edited userValidators to include a regex to check for specific conditions on the password and potentially added logic for confirm password

--- routes/users.js
+++ routes/users.js
@@ -1,55 +1,108 @@
 const express = require('express');
 const router = express.Router();
-const { csrfProtection, asyncHandler } = require('../utils.js');
-const { loginUser } = require('../auth.js');
-const { User } = require('../db/models');
-const { check, validationResult } = require('express-validator');
+const {
+  csrfProtection,
+  asyncHandler
+} = require('../utils.js');
+const {
+  loginUser
+} = require('../auth.js');
+const {
+  User
+} = require('../db/models');
+const {
+  check,
+  validationResult
+} = require('express-validator');
 
 
 
 router.get("/", csrfProtection, asyncHandler(async (req, res) => {
-  if (req.session.auth){
+  if (req.session.auth) {
     res.redirect("/feed");
   } else {
-    res.render("splash", { token: req.csrfToken()});
+    res.render("splash", {
+      token: req.csrfToken()
+    });
   }
 }));
 
-const createUserValidators =[
+const createUserValidators = [
   check('email')
-    .exists({checkFalsy: true})
-    .withMessage('Please input a valid email')
-    .isLength({ max : 100})
-    .withMessage('Please provide an email address under 100 characters'),
+  .exists({
+    checkFalsy: true
+  })
+  .withMessage('Please input a valid email')
+  .isLength({
+    max: 100
+  })
+  .withMessage('Please provide an email address under 100 characters'),
   check('firstName')
-    .exists({checkFalsy: true})
-    .withMessage('Please input a valid first name')
-    .isLength({ max : 45})
-    .withMessage('Please provide a first name under 45 characters'),
+  .exists({
+    checkFalsy: true
+  })
+  .withMessage('Please input a valid first name')
+  .isLength({
+    max: 45
+  })
+  .withMessage('Please provide a first name under 45 characters'),
   check('lastName')
-    .exists({checkFalsy: true})
-    .withMessage('Please input a valid last name')
-    .isLength({ max : 75})
-    .withMessage('Please provide a last name under 75 characters'),
+  .exists({
+    checkFalsy: true
+  })
+  .withMessage('Please input a valid last name')
+  .isLength({
+    max: 75
+  })
+  .withMessage('Please provide a last name under 75 characters'),
+
   check('hashedPassword')
-    .exists({checkFalsy: true})
-    .withMessage('Please input a valid password')
-    .isLength({ max : 255})
-    .withMessage('Please provide a password under 255 characters'),
+  .exists({
+    checkFalsy: true
+  })
+  .withMessage('Please input a valid password')
+  .isLength({
+    max: 255
+  })
+  .withMessage('Please provide a password under 255 characters')
+  .matches(/^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*])/, 'g')
+  .withMessage('Password must contain at least one lowercase letter, one uppercase letter, and one special character(!#%^%$...))'),
+  //confirm password???
+  // check('confirmPassword')
+  // .exists({
+  //   checkFalsy: true
+  // })
+  // .withMessage('Please confirm password')
+  // .custom((value, {
+  //   req
+  // }) => {
+  //   if (value !== req.body.password) {
+  //     throw Error('Passwords do not match')
+  //   }
+  //   return true;
+  // }),
   check('gender')
-    .isLength({ max : 20})
-    .withMessage('Please provide a gender under 20 characters'),
+  .isLength({
+    max: 20
+  })
+  .withMessage('Please provide a gender under 20 characters'),
   check('avatar')
-    .isLength({ max : 255})
-    .withMessage('Please provide an address under 255 characters'),  
+  .isLength({
+    max: 255
+  })
+  .withMessage('Please provide an address under 255 characters'),
 ];
-router.post('/user/create-account', csrfProtection, createUserValidators, asyncHandler( await (req, res) => {
+
+router.post('/user/create-account', csrfProtection, asyncHandler(async (req, res) => {
 
 }))
-
-router.post("/demo-user", csrfProtection, asyncHandler(async(req, res,next) => {
+router.post("/demo-user", csrfProtection, asyncHandler(async (req, res, next) => {
   const email = 'test@test.net';
-  const user = await User.findOne({ where: { email }});
+  const user = await User.findOne({
+    where: {
+      email
+    }
+  });
 
   loginUser(req, res, user);
 
