732c7a930000de0bb3fde21e3d4ac96b2047a4ad
Troy <troyd9216@gmail.com>
2021-03-09 17:39:29 -0600

    completed stories routes

--- routes/stories.js
+++ routes/stories.js
@@ -1,12 +1,15 @@
 const express = require('express');
 const router = express.Router();
+const {
+    csurfProtection
+} = require('../utils')
 
 //GET localhost:8080/stories/submit-story || working
 
-router.get('/submit-story', (req, res) => {
+router.get('/submit-story', csurfProtection, (req, res) => {
     //displays a form to submit a new story
-    res.json({
-        test: 'this is a test get route to stories/submit-story'
+    res.render('pugFile', {
+        csrfToken: req.csrfToken()
     })
 })
 module.exports = router
