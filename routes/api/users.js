59a717e8619b2cc12f05b1a45f80eca9296c2a90
JSchutza <51064354+JSchutza@users.noreply.github.com>
2021-03-10 18:58:20 -0600

    stories api to find all of the storys a user created done

--- routes/api/users.js
+++ routes/api/users.js
@@ -6,11 +6,36 @@ const { asyncHandler } = require('../../utils.js');
 
 
 
-//GET localhost:8080/api/users/:id/bookmarks
-router.get('/:id(\\d+)/bookmarks', asyncHandler( async(req, res) => {
+//GET localhost:8080/api/users/:id/stories
+router.get('/:id(\\d+)/stories', asyncHandler( async(req, res) => {
     //return 5 Stories
     //includes: link to story, quantity of likes, comments
     //organized by current date
+    const userId = req.params.id;
+    const usersStories = await Story.findAll({
+        where: { userId }
+    });
+
+    if(usersStories) {
+        res.json({
+            their_stories: usersStories
+        });
+    } else {
+        res.json({
+            message: "Error, there are no stories associated with this user account."
+        });
+    }
+
+}));
+
+
+
+
+
+
+
+//GET localhost:8080/api/users/:id/bookmarks
+router.get('/:id(\\d+)/bookmarks', asyncHandler( async(req, res) => {
     const userId = req.params.id;
     const usersBookmarks = await User.findByPk(userId, {
         attributes: ["firstName", "lastName", "avatar"],
