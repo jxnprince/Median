ff1286ac90f03e37cf9f6f67d3389bea9d4c9988
Robert George <rgeorge1982@gmail.com>
2021-03-11 10:58:11 -0500

    added some splash page functionality

---
+++ public/JS/splash.js
@@ -0,0 +1,25 @@
+window.addEventListener("DOMContentLoaded", async (event) => {
+  const displayFormButton = document.getElementById('displaySignUpFormButton');
+  const signUpForm = document.getElementById('signUpForm');
+  const demoUserButton = document.getElementById('demoUserButton');
+  const logoDiv = document.getElementById('logoDiv');
+  const backButton= document.getElementById('backToSplash')
+  logoDiv.innerHTML= "I'm a logo placeholder"
+
+  displayFormButton.addEventListener('click', async (event) =>{
+    displayFormButton.setAttribute('class', 'hidden');
+    demoUserButton.setAttribute('class', 'hidden');
+    logoDiv.setAttribute('class', 'hidden');
+    logoDiv.innerHTML='I am hiding!';
+    signUpForm.classList.remove('hidden');
+    console.log('I got clicked');
+  })
+  backButton.addEventListener('click', async (event) =>{
+    displayFormButton.classList.remove('hidden');
+    demoUserButton.classList.remove('hidden');
+    logoDiv.classList.remove('hidden');
+    logoDiv.innerHTML='I back!';
+    signUpForm.setAttribute('class', 'hidden');
+    console.log('I got clicked');
+  })
+});
