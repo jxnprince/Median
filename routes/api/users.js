15fbb8542b42a12f55b58d1f0ce20349a23b6b02
Troy <troyd9216@gmail.com>
2021-03-11 03:01:40 -0600

    completed stories and user api routes

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,26 +1,18 @@
 const express = require('express');
 const router = express.Router();
-const { User } = require('../../db/models');
-const { asyncHandler } = require('../../utils.js');
-
-//GET localhost:8080/api/users/:id/stories || not working ? no id to reference?
-router.get('/', (req, res) => {
-    //return 5 Stories
-    //includes: link to story, quantity of likes, comments
-    //organized by current date
-    res.json({
-        test: "this is a get test to api/users/:id/stories"
-    })
-});
-
-
+const {
+    User
+} = require('../../db/models');
+const {
+    asyncHandler
+} = require('../../utils.js');
 
 //GET localhost:8080/api/users/:id/
 //used for the feed page to get user info for each author on the page
-router.get('/:id(\\d+)', asyncHandler( async (req, res) => {
+router.get('/:id(\\d+)', asyncHandler(async (req, res) => {
     const session = req.session.auth;
 
-    if(session) {
+    if (session) {
         const the_user = await User.findByPk(req.params.id, {
             attributes: ["firstName", "lastName", "avatar"]
         });
@@ -29,7 +21,7 @@ router.get('/:id(\\d+)', asyncHandler( async (req, res) => {
             user: the_user
         });
 
-    }  else {
+    } else {
         res.json({
             message: "You are not authorized.",
             status: 401,
@@ -38,7 +30,5 @@ router.get('/:id(\\d+)', asyncHandler( async (req, res) => {
     }
 
 }));
-
-
 
 module.exports = router;
