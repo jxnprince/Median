5dd97699b591981b2ba344fe1f6f9ed5c432fc0f
JSchutza <51064354+JSchutza@users.noreply.github.com>
2021-06-01 09:42:31 -0500

    db delete on cascade is broken

--- public/JS/single-story.js
+++ public/JS/single-story.js
@@ -11,9 +11,12 @@ window.addEventListener("DOMContentLoaded", async (ev) => {
 	//HTML selectors
 	const mainContentDiv = document.getElementById('Main-Content');
 	const storyObj = document.getElementById('current-story-api-number');
+	const deleteStoryButton = document.getElementById(`delete-story-button`);
+
 
 	//Fetches here:
 	const data = await makeFetch(`/api/stories/${storyObj.className}`);
+
 
 
 	mainContentDiv.innerHTML = `
@@ -29,6 +32,14 @@ window.addEventListener("DOMContentLoaded", async (ev) => {
 				</div>
 
 	</div>`;
+
+
+	deleteStoryButton.addEventListener("click", async (event) => {
+		const response = await fetch(`/api/stories/${storyObj.className}`, {
+			method: "DELETE",
+			credentials: "include",
+		});
+	});
 
 
 
