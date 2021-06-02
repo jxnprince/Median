fbe4a420870ec015fbf37bf74b16566bbd213970
Robert George <rgeorge1982@gmail.com>
2021-06-02 13:20:29 -0400

    working on surfacing comments

--- public/JS/single-story.js
+++ public/JS/single-story.js
@@ -13,11 +13,31 @@ window.addEventListener("DOMContentLoaded", async (ev) => {
 	const storyObj = document.getElementById('current-story-api-number');
 	const deleteStoryButton = document.getElementById(`delete-story-button`);
 
-
 	//Fetches here:
 	const data = await makeFetch(`/api/stories/${storyObj.className}`);
+	const comments = await makeFetch(`/api/comments/${storyObj.className}`)
+	const users = []
+	//  = comments.map((comment)=> {
+		// 	console.log('%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%', comment)
+		// 	// const user = await makeFetch(`/api/users/${comment.userId}/`)
+		// 	// return user;
+		
+		// })
+		for (let i = 0; i < comments.length; i++){
+		console.log('############################', comments[i])
+		const user = await makeFetch(`/api/users/${comments[i].userId}/`)
+		users.push(user);
+	}
+	console.log('$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$', comments, users)
+	const userObj = {}
+	user.forEach(user =>{
+		userObj[user.userId] = user
+	})
 
-
+	let commentsDiv = ''
+	for (let i = 0; i < comments.length; i++){
+		commentDiv += `<div class="comment"><p>${comment[i].body}</p><span>$${userObj[comment[i].userId]}</div>`
+	}
 
 	mainContentDiv.innerHTML = `
 	<div class= 'Container'>
@@ -29,6 +49,9 @@ window.addEventListener("DOMContentLoaded", async (ev) => {
 					<div class= 'Story-body'>
 						<p>${data.postBody}</p>
 					</div>
+				</div>
+				<div class='comments-container>
+
 				</div>
 
 	</div>`;
