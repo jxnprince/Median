19721eb6b4de8432dae3d2b334656a757c5ee190
JSchutza <joshua.schutza@pop.belmont.edu>
2021-08-11 11:20:19 -0500

    added more boiler plate code

---
+++ frontend/src/actions/session.js
@@ -0,0 +1,67 @@
+// holds all of the action creators for the session
+
+// import types here
+import { LOGIN_USER, CHECK_USER, LOGOUT_USER, SIGNUP_USER, LOGIN_DEMO } from '../types/session.js';
+
+// action creators here
+const checkForUser = (the_user) => {
+  return {
+    type: CHECK_USER,
+    user: the_user
+  };
+};
+
+
+
+
+
+
+const loginUser = (the_user) => {
+  return {
+    type: LOGIN_USER,
+    user: the_user
+  };
+};
+
+
+
+const logoutUser = (the_user) => {
+  return {
+    type: LOGOUT_USER,
+    user: the_user
+  };
+};
+
+
+
+
+const signupUser = (the_user) => {
+  return {
+    type: SIGNUP_USER,
+    user: the_user
+  };
+}
+
+
+
+
+const loginDemoUser = (the_user) => {
+  return {
+    type: LOGIN_DEMO,
+    user: the_user
+  };
+}
+
+
+
+
+// exports here
+export {
+  checkForUser,
+  loginUser,
+  logoutUser,
+  signupUser,
+  loginDemoUser,
+
+
+};
---
+++ frontend/src/thunks/session.js
@@ -0,0 +1,114 @@
+// holds all the thunk functions for the session
+
+// import action creators here
+import { loginUser, checkForUser, logoutUser, signupUser, loginDemoUser } from '../actions/session.js';
+
+// import csrfFetch here
+import { csrfFetch } from '../store/csrf.js';
+
+
+
+
+
+
+// thunks here
+const thunk_checkIfThereIsAUser = () => async (dispatch) => {
+  const response = await csrfFetch('/api/users/');
+
+  if (response.ok) {
+    const the_user = await response.json();
+    dispatch(checkForUser(the_user));
+    return;
+  }
+  throw response;
+};
+
+
+
+
+
+
+
+
+
+
+
+
+const thunk_login = ({ credential, password }) => async (dispatch) => {
+
+  const response = await csrfFetch('/api/users/login', {
+    method: 'POST',
+    body: JSON.stringify({ credential, password })
+  });
+
+  if (response.ok) {
+    const the_user = await response.json();
+    dispatch(loginUser(the_user));
+    return;
+  }
+  throw response;
+};
+
+
+
+
+const thunk_logoutUser = () => async (dispatch) => {
+  const response = await csrfFetch('/api/users/logout', { method: 'DELETE' });
+
+  if (response.ok) {
+    const the_user = await response.json();
+    dispatch(logoutUser(the_user));
+    return;
+  }
+  throw response;
+};
+
+
+
+
+const thunk_signupUser = ({ email, password, username }) => async (dispatch) => {
+
+  const response = await csrfFetch('/api/users/signup', {
+    method: 'POST',
+    body: JSON.stringify({ email, password, username })
+  });
+
+  if (response.ok) {
+    const the_user = await response.json();
+    dispatch(signupUser(the_user));
+    return;
+  }
+  throw response;
+};
+
+
+
+const thunk_loginDemoUser = () => async (dispatch) => {
+  const credential = 'demo-user';
+  const password = 'password';
+
+  const response = await csrfFetch('/api/users/login', {
+    method: 'POST',
+    body: JSON.stringify({ credential, password })
+  });
+
+  if (response.ok) {
+    const the_user = await response.json();
+    dispatch(loginDemoUser(the_user));
+    return;
+  }
+  throw response;
+};
+
+
+
+// exports here
+export {
+  thunk_checkIfThereIsAUser,
+  thunk_login,
+  thunk_logoutUser,
+  thunk_signupUser,
+  thunk_loginDemoUser,
+
+
+};
