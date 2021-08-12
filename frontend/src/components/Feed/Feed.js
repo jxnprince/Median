2182b6d11c9bd59b8345abb57a9206235bc04684
JSchutza <joshua.schutza@pop.belmont.edu>
2021-08-11 22:12:48 -0500

    updates to Feed component

--- frontend/src/components/Feed/Feed.js
+++ frontend/src/components/Feed/Feed.js
@@ -1,6 +1,6 @@
 
 import { useEffect } from 'react';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 
 import styles from "./feed.module.css";
 
@@ -13,6 +13,8 @@ import { useUser } from '../../context/UserContext';
 const Feed = () => {
   const dispatch =  useDispatch();
   const { isUser } = useUser();
+  const stories = useSelector(store => store.feedReducer.featuredStories);
+
 
 
   useEffect(() => {
@@ -21,18 +23,32 @@ const Feed = () => {
 
 
 
-  return (
-    <>
-    {/* first main story  */}
-      <div className={'featured'}>
-        <div className={styles.featured_container}>
-
+  if(stories !== null){
+    return (
+      <>
+      {/* first main story  */}
+        <div className={'featured'}>
+          <div className={styles.featured_container}>
+            {Object.values(stories).map(story => (
+              <>
+                <h1>{story.title} </h1>
+              </>
+            ))}
+          </div>
         </div>
-      </div>
 
 
-      {/* all of the other stories */}
-    </>
+        {/* all of the other stories */}
+      </>
+    )
+
+  }
+
+
+  return (
+      <>
+        <h1>Loading Feed...</h1>
+      </>
   )
 
 
