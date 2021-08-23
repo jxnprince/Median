8ac200cdc7d77330cfbe4ac6ed7e0edc5758e49c
JSchutza <51064354+JSchutza@users.noreply.github.com>
2021-08-23 15:39:39 -0700

    displaying basic followers info on profile page

--- frontend/src/components/Profile/Profile.js
+++ frontend/src/components/Profile/Profile.js
@@ -1,5 +1,11 @@
-
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import { useUser } from "../../context/UserContext";
+
+import { Link } from "react-router-dom";
+
+
+import { thunk_getFollowers } from "../../thunks/profile.js";
 
 
 
@@ -7,6 +13,15 @@ import { useUser } from "../../context/UserContext";
 
 const Profile = () => {
   const { isUser } = useUser();
+  const dispatch = useDispatch();
+  const followers = useSelector(store => store.followersReducer.stories);
+
+
+  useEffect(() => {
+    dispatch(thunk_getFollowers(isUser.id));
+  },[dispatch]);
+
+
 
 
 
@@ -23,6 +38,26 @@ const Profile = () => {
         <h3>{isUser.birthdate}</h3>
         <h3>{isUser.email}</h3>
       </div>
+
+
+      <div>
+        {followers !== null ?
+          <>
+            {Object.values(followers).map(eachFollower => (
+              <>
+                <div>
+                  <Link to={`/story/${eachFollower.eachStory.id}`} >
+                    <img src={eachFollower.eachStory.imgUrl } />
+                  </Link>
+                </div>
+              </>
+            ))}
+          </>
+          :
+          <></>
+        }
+      </div>
+
     </>
   )
 
