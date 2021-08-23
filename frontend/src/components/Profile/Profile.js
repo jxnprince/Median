7af3cb3fb425f6e2f3a24619e1cc76d9287f72d3
JSchutza <51064354+JSchutza@users.noreply.github.com>
2021-08-23 15:57:48 -0700

    started to add styles to profile page

--- frontend/src/components/Profile/Profile.js
+++ frontend/src/components/Profile/Profile.js
@@ -8,6 +8,7 @@ import { Link } from "react-router-dom";
 import { thunk_getFollowers } from "../../thunks/profile.js";
 
 
+import styles from "./profile.module.css";
 
 
 
@@ -27,15 +28,15 @@ const Profile = () => {
 
   return (
     <>
-      <div>
-        <img src={isUser.avatar} />
+
+      <div className={styles.profile_img}>
+          <img src={isUser.avatar} className={styles.mainProfileImg} />
       </div>
 
-      <div>
+      <div className={styles.userinfo}>
         <h3>{isUser.firstName}</h3>
         <h3>{isUser.lastName}</h3>
         <h3>{isUser.gender}</h3>
-        <h3>{isUser.birthdate}</h3>
         <h3>{isUser.email}</h3>
       </div>
 
@@ -43,11 +44,14 @@ const Profile = () => {
       <div>
         {followers !== null ?
           <>
+            <h1>Your Followers {Object.values(followers).length} </h1>
+
             {Object.values(followers).map(eachFollower => (
               <>
                 <div>
                   <Link to={`/story/${eachFollower.eachStory.id}`} >
                     <img src={eachFollower.eachStory.imgUrl } />
+
                   </Link>
                 </div>
               </>
