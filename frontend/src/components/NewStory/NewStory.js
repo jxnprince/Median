29672b2484c4c10e4c0073d913103c30b528d43b
JSchutza <51064354+JSchutza@users.noreply.github.com>
2021-08-24 19:26:44 -0700

    updates to story creator component

--- frontend/src/components/NewStory/NewStory.js
+++ frontend/src/components/NewStory/NewStory.js
@@ -1,19 +1,28 @@
 import { useState, useEffect } from 'react';
 import { useDispatch } from 'react-redux';
+import { useHistory } from 'react-router-dom';
 
 
 
 
 
 const NewStory = () => {
+  // imgUrl, postBody, title
+  const [ imgUrl, setImgUrl ] = useState('');
+  const [ postBody, setPostBody ] = useState('');
   const [ title, setTitle ] = useState('');
   const dispatch = useDispatch();
+  const history = useHistory();
 
 
 
 
   const onSubmit = event => {
     event.preventDefault();
+    //dispatch thunk here
+    
+    history.push('/profile');
+
   }
 
 
@@ -24,6 +33,23 @@ const NewStory = () => {
     <>
     <form onSubmit={onSubmit}>
       <label>
+        Image
+        <input
+          type='text'
+          value={imgUrl}
+          onChange={event => setImgUrl(event.target.value)}
+        />
+      </label>
+
+      <label>
+        Story Text
+        <textarea
+          value={postBody}
+          onChange={event => setPostBody(event.target.value)}
+        />
+      </label>
+
+      <label>
         Title
         <input
           type='text'
@@ -31,6 +57,7 @@ const NewStory = () => {
           onChange={event => setTitle(event.target.value)}
         />
       </label>
+
 
 
       <button type='submit'> Create </button>
