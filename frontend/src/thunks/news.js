204a7a0c619209c02df5ead1eb7dbd0f8e3e2dc9
JSchutza <51064354+JSchutza@users.noreply.github.com>
2021-08-25 17:55:20 -0700

    displaying news data on splash page

--- frontend/src/thunks/news.js
+++ frontend/src/thunks/news.js
@@ -11,7 +11,8 @@ import { getNews } from "../actions/news.js";
 const thunk_getNews = () => async (dispatch) => {
   const response = await fetch('https://newsapi.org/v2/everything?q=tesla&pageSize=5&apiKey=3544f9dee45945b383830e6b1adfdd3c');
   if(response.ok) {
-    const news = await response.json();
+    const data = await response.json();
+    const news = { news: data.articles };
     dispatch(getNews(news));
     return;
   }
