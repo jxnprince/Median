18d0da8748ce893c573e9d2941409f36b38dffa9
JSchutza <51064354+JSchutza@users.noreply.github.com>
2021-08-26 09:01:59 -0700

    added api route to bring all the stories a logged in user has created for their profile page

--- backend/routes/stories.js
+++ backend/routes/stories.js
@@ -8,8 +8,8 @@ const router = express.Router();
 
 
 
-
-// GET localhost:8080/api/stories/
+// shows the most recently created stories and set a limit to 5
+// GET localhost:5000/api/stories/
 router.get('/', asyncHandler(async (request, response) => {
 
     const mostRecentStories = await Story.findAll({ order: [['createdAt']], limit: 5 });
@@ -25,7 +25,27 @@ router.get('/', asyncHandler(async (request, response) => {
         }
     })
 )
-//return a list of the most recent stories
+
+
+
+// get all of the stories created by a logged in user
+// GET localhost:5000/api/stories/:userId
+router.get('/:userId(\\d+)', asyncHandler(async (request, response) => {
+    const userId = request.params.userId;
+    const stories = await Story.findAll({
+        where: { userId },
+        order: [['createdAt']]
+    });
+
+    // normalize the backend
+    const result = {};
+    stories.forEach(eachStory => {
+        result[eachStory.id] = eachStory;
+    });
+
+
+    response.json({ stories: result });
+}));
 
 
 
@@ -48,10 +68,10 @@ router.post('/:userId(\\d+)', asyncHandler(async (request, response) => {
 
 
 
+// get a specific story by its storyId
 
-
-//GET localhost:5000/api/stories/:id
-router.get('/:id(\\d+)', asyncHandler(async (request, response) => {
+//GET localhost:5000/api/stories/specific/:id
+router.get('/specific/:id(\\d+)', asyncHandler(async (request, response) => {
     const storyId = request.params.id;
     const userStories = await Story.findByPk(storyId, {
         include: {
