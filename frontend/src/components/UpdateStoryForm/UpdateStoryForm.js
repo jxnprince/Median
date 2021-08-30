1af6935fa4a5864c77d8a57f83631660db2cda60
JSchutza <51064354+JSchutza@users.noreply.github.com>
2021-08-30 09:06:02 -0700

    started / added update story form component

---
+++ frontend/src/components/UpdateStoryForm/UpdateStoryForm.js
@@ -0,0 +1,69 @@
+
+
+import { useState, useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import { useHistory } from 'react-router-dom';
+
+
+
+
+const UpdateStoryForm = () => {
+  const [imgUrl, setImgUrl] = useState('');
+  const [postBody, setPostBody] = useState('');
+  const [title, setTitle] = useState('');
+  const dispatch = useDispatch();
+  const history = useHistory();
+
+
+
+
+  const onSubmit = event => {
+    event.preventDefault();
+    const payload = { imgUrl, postBody, title };
+    // dispatch();
+    history.push('/profile');
+  };
+
+
+
+
+
+  return (
+    <>
+      <form onSubmit={onSubmit}>
+        <label>
+          Image
+          <input
+            type='text'
+            value={imgUrl}
+            onChange={event => setImgUrl(event.target.value)}
+          />
+        </label>
+
+        <label>
+          Story Text
+          <textarea
+            value={postBody}
+            onChange={event => setPostBody(event.target.value)}
+          />
+        </label>
+
+        <label>
+          Title
+          <input
+            type='text'
+            value={title}
+            onChange={event => setTitle(event.target.value)}
+          />
+        </label>
+
+
+
+        <button type='submit'> Create </button>
+      </form>
+    </>
+  )
+};
+
+
+export default UpdateStoryForm;
