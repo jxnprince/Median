fecd88f3070a03427bad43671b274e835d4d3295
JSchutza <51064354+JSchutza@users.noreply.github.com>
2021-08-30 08:49:32 -0700

    added update story component

---
+++ frontend/src/components/UpdateStoryButton/UpdateStoryButton.js
@@ -0,0 +1,31 @@
+
+
+
+
+import { useState } from 'react';
+import { useDispatch } from 'react-redux';
+
+
+
+
+
+const UpdateStoryButton = () => {
+  const dispatch = useDispatch();
+
+
+  const handleUpdate = event => {
+    event.preventDefault();
+    //dispatch here
+  }
+
+
+  return (
+    <>
+      <button onClick={event => handleUpdate(event)} > Update </button>
+    </>
+  );
+
+};
+
+
+export default UpdateStoryButton;
