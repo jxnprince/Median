4afd718c015465743260ed7ed0a2d0fcd195e055
JSchutza <51064354+JSchutza@users.noreply.github.com>
2021-09-03 16:15:43 -0700

    working on signup errors

--- frontend/src/reducers/session.js
+++ frontend/src/reducers/session.js
@@ -2,7 +2,7 @@
 
 
 // import types here
-import { LOGIN_USER, CHECK_USER, LOGOUT_USER, LOGIN_DEMO } from '../types/session.js';
+import { LOGIN_USER, CHECK_USER, LOGOUT_USER, LOGIN_DEMO, SIGNUP_USER } from '../types/session.js';
 
 
 
@@ -14,12 +14,19 @@ const userReducer = (state = { user: null }, action) => {
   switch (action.type) {
     case CHECK_USER:
       return { ...state, ...action.user };
+
     case LOGIN_USER:
       return { ...state, ...action.user };
+
     case LOGOUT_USER:
       return { ...state, ...action.user };
+
     case LOGIN_DEMO:
       return { ...state, ...action.user };
+
+    case SIGNUP_USER:
+      return { ...state, ...action.user };
+
     default:
       return state;
   }
